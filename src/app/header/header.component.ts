a269dad8e0ee352aed574784f3af6e5cfecf5dc2
Adnan Nassar <mohamad-adnan.nassar@adesso.de>
2020-07-19 21:04:56 +0200

    .

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {outputPath} from '@angular-devkit/build-angular/src/test-utils';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  @Output() pageSelected  =  new EventEmitter<string>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  onSelect(page: string) {
+    this.pageSelected.emit(page);
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {computeLineStartsMap} from '@angular/core/schematics/utils/line_mappings';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,15 +8,18 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes : Recipe[] =  [
-    new Recipe('Test Recipe' , 'this is a Test Recipe',
+   recipes : Recipe[] =  [
+    new Recipe('Test Recipe 1' , 'this is a Test Recipe 1',
       'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg' ),
-    new Recipe('Test Recipe' , 'this is a Test Recipe',
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg' )
-  ];
+    new Recipe('Test Recipe 2' , 'this is a Test Recipe 2',
+      'https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg')];
+  @Output() selectdRecipe  = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(selectedRecipe: Recipe) {
+    this.selectdRecipe.emit(selectedRecipe);
+  }
 }
